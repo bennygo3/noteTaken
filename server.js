b14753898bb1ce40879d22f2d7a2d2d9bedc708e
bennygo3 <3gobenjamin@gmail.com>
2022-03-04 09:27:16 -0600

    need to finish figuring out delete button

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const path = require("path");
 const db = require("./db/db.json");
-const fs = require("fs")
+const fs = require("fs");
+const { randomUUID } = require("crypto");
 
 const PORT = process.env.PORT || 3001;
 const app = express()
@@ -12,29 +13,41 @@ app.use(express.static("public"))
 
 //routing
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
 })
 
-// Lazy way, don't judge
-// app.get("/api/notes", (req, res) => {
-//     res.json(db)
-// })
+
 
 app.get("/api/notes", (req, res) => {
-    fs.readFile("./db/db.json", (err, data) => {
-        if(err) throw err;
-        const dbInfo = JSON.parse(data)
-        res.json(dbInfo)
-    })
-})
+    // fs.readFile("./db/db.json", (err, data) => {
+    //     if(err) throw err;
+    //     const dbInfo = JSON.parse(data)
+    res.json(db)
+    // })
+});
 
 //POST
+app.post("/api/notes", (req, res) => {
+    let newNote = req.body;
+    newNote.id = randomUUID() 
+    db.push(newNote);
+    fs.writeFile("./db/db.json", JSON.stringify(db, null, 4), (err) => {
+        err ? console.log(err) : res.send(newNote)
+    })
+
+});
+
+// app.delete("/api/notes/:id", (req, res) => {
+//     const noteId = req.params.id;
+//     res.unlink(noteId, './db/db.json')
+//     res.readFile(noteId, './db/db.json')
+// });
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
 app.listen(PORT, (err) => {
-    if(err) throw err;
-    console.log("runningin the 90s")
+    if (err) throw err;
+    console.log("up and running")
 })
